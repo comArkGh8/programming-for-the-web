f2c4746d4d66b5340fab5600b3e0f1be4951fa84
saul <comarkgh8@gmail.com>
2019-05-17 14:44:37 +0200

    initial setup of calculator

--- week2/HW 3/calc.js
+++ week2/HW 3/calc.js
@@ -1,3 +1,22 @@
-/*
- * Implement all your JavaScript in this file!
- */
+
+
+var outDisplay = $("#output");
+var calcWindow = $("#display");
+
+function numberButton() {
+  outDisplay.html('hi');
+  var currValue = calcWindow.val();
+  var newValue = currValue + $(this).val();
+  calcWindow.val(newValue);
+};
+
+function buttonHandler(b){
+  if (b.value == '1'){
+    numberButton();
+  }
+  else{
+    // then it is an operation
+  }
+};
+
+$("button").click(buttonHandler);
