cf70c2c0469e61e72fdb5feff9fce154603d483d
saul <comarkgh8@gmail.com>
2019-05-17 15:46:51 +0200

    addition works

--- week2/HW 3/calc.js
+++ week2/HW 3/calc.js
@@ -3,20 +3,64 @@
 var outDisplay = $("#output");
 var calcWindow = $("#display");
 
-function numberButton() {
-  outDisplay.html('hi');
-  var currValue = calcWindow.val();
-  var newValue = currValue + $(this).val();
-  calcWindow.val(newValue);
-};
+var disp;
+var oldValue;
+var operation = '';
 
-function buttonHandler(b){
-  if (b.value == '1'){
-    numberButton();
-  }
-  else{
-    // then it is an operation
-  }
-};
 
-$("button").click(buttonHandler);
+$("button").click(function() {
+  var currDisp = calcWindow.val();
+  disp = currDisp + $(this).val();
+  calcWindow.val(disp);
+});
+
+
+$("#addButton").click(function() {
+  oldValue = calcWindow.val();
+  operation = '+';
+  disp = null;
+  calcWindow.val(disp);
+  outDisplay.html('hi add');
+});
+
+$("#subtractButton").click(function() {
+  oldValue = calcWindow.val();
+  operation = '-';
+  outDisplay.html('hi sub');
+});
+
+$("#multiplyButton").click(function() {
+  oldValue = calcWindow.val();
+  operation = '*';
+  outDisplay.html('hi mult');
+});
+
+$("#clearButton").click(function() {
+  outDisplay.html('hi clear');
+});
+
+$("#equalsButton").click(function() {
+  var newValue;
+  switch (operation){
+    case '':
+      break;
+    case '+':
+      newValue = Number(oldValue) + Number(disp);
+      calcWindow.val(newValue);
+      break;
+    case '-':
+      break;
+    case '*':
+      break;
+    case '/':
+      break;
+  }
+  operation = '';
+
+});
+
+$("#divideButton").click(function() {
+  oldValue = calcWindow.val();
+  operation = '/';
+  outDisplay.html('hi divide');
+});
