1a2425b66e40d5e01695502d593a920fd8a0626a
comarkgh8 <comarkgh8@gmail.com>
2019-06-30 17:29:27 +0200

    checkbx now functioning

--- week3/HW 4/FontChooser.js
+++ week3/HW 4/FontChooser.js
@@ -3,15 +3,21 @@ class FontChooser extends React.Component {
     constructor(props) {
 	     super(props);
        this.state = { hidden : 'true'};
+       this.state = { boldCheck : this.props.bold};
     }
 
     handleClick() {
       this.setState( { hidden : (this.state.hidden == 'true' ? 'false' : 'true') } );
     }
 
+    handleCheck() {
+      this.setState( { boldCheck : (this.state.boldCheck == 'true' ? 'false' : 'true') } );
+    }
+
+
 
     render() {
-      var isBold = (this.props.bold == 'true');
+      var isBold = (this.state.boldCheck == 'true');
       var bold = isBold ? 'bold' : 'normal';
       var size = this.props.size;
       const style = (this.state.hidden == 'true')? {display: 'none'} : {};
@@ -19,14 +25,15 @@ class FontChooser extends React.Component {
       return(
     	       <div>
 
-      	       <input type="checkbox" id="boldCheckbox" style={style}/>
+      	       <input type="checkbox" id="boldCheckbox" style={style}
+                      onChange={this.handleCheck.bind(this)}/>
       	       <button id="decreaseButton" style={style}>-</button>
       	       <span id="fontSizeSpan" style={style}>{this.props.size}</span>
       	       <button id="increaseButton" style={style}>+</button>
                <span id="textSpan" style={{fontSize:size, fontWeight:bold}} onClick={this.handleClick.bind(this)}>
                   {this.props.text}
                </span>
-               <li> {this.state.hidden} </li>
+               <li> {this.state.boldCheck} </li>
 
 
     	       </div>
