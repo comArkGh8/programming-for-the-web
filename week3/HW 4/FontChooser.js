0684b7fde02f90c3da5805c3c2acd7347e3b6064
comarkgh8 <comarkgh8@gmail.com>
2019-06-30 15:06:35 +0200

    can hide and show form

--- week3/HW 4/FontChooser.js
+++ week3/HW 4/FontChooser.js
@@ -1,21 +1,35 @@
 class FontChooser extends React.Component {
 
     constructor(props) {
-	super(props);
+	     super(props);
+       this.state = { hidden : 'true'};
     }
-    
+
+    handleClick() {
+      this.setState( { hidden : (this.state.hidden == 'true' ? 'false' : 'true') } );
+    }
+
 
     render() {
+      var isBold = (this.props.bold == 'true');
+      var bold = isBold ? 'bold' : 'normal';
+      var size = this.props.size;
+      const style = (this.state.hidden == 'true')? {display: 'none'} : {};
 
-	return(
-	       <div>
-	       <input type="checkbox" id="boldCheckbox" hidden='true'/>
-	       <button id="decreaseButton" hidden='true'>-</button>
-	       <span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-	       <button id="increaseButton" hidden='true'>+</button>
-	       <span id="textSpan">{this.props.text}</span>
-	       </div>
-	);
+      return(
+    	       <div>
+
+      	       <input type="checkbox" id="boldCheckbox" style={style}/>
+      	       <button id="decreaseButton" style={style}>-</button>
+      	       <span id="fontSizeSpan" style={style}>{this.props.size}</span>
+      	       <button id="increaseButton" style={style}>+</button>
+               <span id="textSpan" style={{fontSize:size, fontWeight:bold}} onClick={this.handleClick.bind(this)}>
+                  {this.props.text}
+               </span>
+               <li> {this.state.hidden} </li>
+
+
+    	       </div>
+    	);
     }
 }
-
